f50d332473a916e8b9a0913a38378edaf36a73ec
SakuretsuKumare <78335074+SakuretsuKumare@users.noreply.github.com>
2021-04-20 15:44:48 -0400

    Tested some Particle System stuff
    I messed around with the fireball stuff a bit in David Scene.

---
+++ Assets/Scripts/CastSpell.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This code was used from https://www.youtube.com/watch?v=wBMrVrdea6s
+
+public class CastSpell : MonoBehaviour
+{
+    public Rigidbody spell;
+    public float throwSpeed;
+    [SerializeField] GameObject Burst;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spell.velocity = transform.forward * throwSpeed;
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(Burst, transform.position, transform.rotation);
+
+        /*if (collision.transform.tag == "Enemy")
+        {
+            // Get comp for enemy script
+            // Enemy - damage from health
+        }*/
+
+        Destroy(this.gameObject);
+    }
+}
