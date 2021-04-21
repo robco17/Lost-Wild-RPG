735fa5822f0bf0e8a5853310d4725c80f54c301d
cgulick77 <cgulick@student.fitchburgstate.edu>
2021-04-20 23:53:04 -0400

    updated main scene
    added more houses

--- Assets/Scripts/TPSShooter.cs
+++ Assets/Scripts/TPSShooter.cs
@@ -31,15 +31,15 @@ public class TPSShooter : MonoBehaviour
 
     void ShootProjectile()
     {
-        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f,0.5f,0));
-        RaycastHit hit;
+           /* Ray ray = cam.ViewportPointToRay(new Vector3(0.5f,0.5f,0));
+            RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit))
-            destination = hit.point;
-            else
-            destination = ray.GetPoint(1000);
-        
-        InstantiateProjectile(firePoint);
+            if (Physics.Raycast(ray, out hit))
+                destination = hit.point;
+                else
+                destination = ray.GetPoint(1000);
+            
+           */ InstantiateProjectile(firePoint);
     }
 
     void InstantiateProjectile(Transform firePoint)
