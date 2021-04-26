11843fb0b80af38a40b764c2b34dc1a7dba47b29
SakuretsuKumare <78335074+SakuretsuKumare@users.noreply.github.com>
2021-04-25 20:08:07 -0400

    Tried to Implement Fungus
    Still needs some work. I was using Simon NPC for the testing of Fungus.

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TriggerDialogue(string npc)
+    {
+        if (npc.Equals("Victor"))
+        {
+            Fungus.Flowchart.BroadcastFungusMessage("Victor Start");
+        }
+        else if (npc.Equals("Claire"))
+        {
+            Fungus.Flowchart.BroadcastFungusMessage("Claire Start");
+        }
+        else if (npc.Equals("Grandma"))
+        {
+            Fungus.Flowchart.BroadcastFungusMessage("Grandma Start");
+        }
+        else if (npc.Equals("Simon"))
+        {
+            Fungus.Flowchart.BroadcastFungusMessage("Simon Start");
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TriggerDialogue(gameObject.tag);
+        }
+    }
+}
